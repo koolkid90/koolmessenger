d2f255e7e02d1a225e740081d44cbddb659fd8ee
koolkid90 <coldflashblur@yandex.ru>
2026-02-16 13:42:30 +0300

    Обновлены комментарии

--- php/cipher.php
+++ php/cipher.php
@@ -4,7 +4,7 @@
 
 // переменнные шифрования
 
-// iv 
+// iv . пока нулевые байты для дальнейшей модификации
 $iv = chr(0x1) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0);
 // пароль, который перемешивается алгоритмом SHA-256
 $password = substr(hash('sha256', '3sc3RLrpd17', true), 0, 32);
